7b2d06646d705470cb4a7d91f946e79d855bd973
Jordan Ross <ross3jc@mail.uc.edu>
2018-01-30 10:01:28 -0500

    Start parser

--- src/parser.cpp
+++ src/parser.cpp
@@ -45,6 +45,147 @@ Parser::Parser(Scanner* scan) : scanner(scan) { }
 
 void Parser::parse() 
 {
-    scanner_debug(scanner);
+    //scanner_debug(scanner);
+    Token token = scanner->getToken();
+    if (token.type == TokenType::RS_PROGRAM) 
+    {
+        program();
+    }
 }
+
+void Parser::program()
+{
+    Token id = scanner->getToken();
+    if (id.type == TokenType::IDENTIFIER)
+    {
+        char* program_name = id.val.string_value;
+        std::cout << "Prog. name is " << program_name << '\n';
+    }
+}
+void Parser::program_header()
+{
+}
+
+void Parser::program_body()
+{
+}
+
+void Parser::identifier()
+{
+}
+
+void Parser::declaration()
+{
+}
+
+
+void Parser::proc_declaration()
+{
+}
+
+void Parser::proc_header()
+{
+}
+
+void Parser::proc_body()
+{
+}
+
+void Parser::parameter_list()
+{
+}
+
+void Parser::parameter()
+{
+}
+
+
+void Parser::var_declaration()
+{
+}
+
+void Parser::type_mark()
+{
+}
+
+void Parser::lower_bound()
+{
+}
+
+void Parser::upper_bound()
+{
+}
+
+
+void Parser::statement()
+{
+}
+
+void Parser::assignment_statement()
+{
+}
+
+void Parser::if_statement()
+{
+}
+
+void Parser::loop_statement()
+{
+}
+
+void Parser::return_statement()
+{
+}
+
+void Parser::proc_call()
+{
+}
+
+
+void Parser::argument_list()
+{
+}
+
+void Parser::destination()
+{
+}
+
+
+void Parser::expression()
+{
+}
+
+void Parser::arith_op()
+{
+}
+
+void Parser::relation()
+{
+}
+
+void Parser::term()
+{
+}
+
+void Parser::factor()
+{
+}
+
+void Parser::name()
+{
+}
+
+void Parser::number()
+{
+}
+
+void Parser::string_literal()
+{
+}
+
+void Parser::char_literal()
+{
+}
+
+
 
--- src/parser.h
+++ src/parser.h
@@ -8,5 +8,44 @@ public:
     void parse();
 private:
     Scanner* scanner;
+    void program();
+    void program_header();
+    void program_body();
+
+    void identifier();
+    void declaration();
+
+    void proc_declaration();
+    void proc_header();
+    void proc_body();
+    void parameter_list();
+    void parameter();
+
+    void var_declaration();
+    void type_mark();
+    void lower_bound();
+    void upper_bound();
+
+    void statement();
+    void assignment_statement();
+    void if_statement();
+    void loop_statement();
+    void return_statement();
+    void proc_call();
+
+    void argument_list();
+    void destination();
+
+    void expression();
+    void arith_op();
+    void relation();
+    void term();
+    void factor();
+    void name();
+    void number();
+    void string_literal();
+    void char_literal();
+
+
 };
 
