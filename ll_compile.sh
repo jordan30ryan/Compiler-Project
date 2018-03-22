a9a0d20fd5c4753da637e14f939339a5abc9c3fb
Jordan Ross <jordanross909@gmail.com>
2018-03-22 11:16:26 -0400

    Update ll compiler script

--- ll_compile.sh
+++ ll_compile.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 filename="${1%.*}"
+outfile="${filename##*/}.out"
+
 llc "${filename}.ll"
-clang "${filename}.s" ./src/runtime/runtime.c
+clang "${filename}.s" ./src/runtime/runtime.c -o $outfile
+
+#Remove intermediates
+rm "${filename}.ll"
+rm "${filename}.s"
 
